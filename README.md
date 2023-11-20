4697af5d9aa1892e133a801494b3a6f6b643f70d
James Mulcahy <james@manque.net>
2023-11-19 22:28:43 -0800

    Initial AC-MAX-24 Implementation

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # hass-acmax24
-Home Assistant integration for the AVPro Edge AC-MAX-24 Audio Matrix
+
+Home Assistant integration for the [AVPro Edge AC-MAX-24](https://avproedge.com/products/ac-max-24) Audio Matrix. If you do not have one of these devices, then this integration is likely of no use to you.
+
+## Configuration
+
+This is only configurable through `configuration.yaml`, but the only configuration that is necessary is the Hostname or IP of the AC-MAX-24. All other configuration is read directly from the AC-MAX-24 device. It's possible to support configuration through the UI
+by implementing the "config flow" mechanism -- PRs are welcome.
+
+To enable this integration, you'll need to install this repo into your config/custom_components directory.  Additionally, you must add the following configuration to your `configuration.yaml`.
+```
+media_player:
+  - platform: acmax24
+    host: your.hostname.or.ip.here.com
+```
+
+## Behavior
+
+This integration uses the AC-MAX-24 "uart" websocket API, in addition to the "cmd" HTTP API. I've not found a way to get the same information out of both APIs. PRs are welcome. The websocket uart is used to monitor the inputs and outputs, and their current state. The "cmd" HTTP API is used to read out the labels/names for all the inputs/outputs (this is the only state which is pulled from that API).
+
+Each _enabled_ AC-MAX-24 output becomes a Media Player in Home Assistant. Each _enabled_ intput becomes a source which is selectable
+in each of the Media Player entities. All disabled outputs are ignored. Enabling or disabling outputs after the integration has started up, is not supported. It will not make Home Assistant aware of those changes. You must reload the integration to pick up any changes to enabled/disabled states.
+
+Muting, Unmuting, Selecting Sources, and adjusting volume (both in absolute terms, and stepping up and down) are supported by this integration.
+
+## Bugs
+
+This is my first home assistant integration. This should not be considered "production quality" software; it has no tests, and has
+only been manually tested in a single environment (my home). There are likely many bugs, but the known issues at this time are:
+
+- The websocket API will frequently return 'CMD ERROR' in response to requests -- likely because we're not honoring the protocol correctly. As a workaround, all commands are sent twice, this usually means that one or both of the requests succeed. Unfortunately, for non-idempotetnt updates (such as volume step changes), this can mean they are applied twice.
+
+## Limitations
+
+- This integration assumes you're running the AC-MAX-24 in insecure mode, if you have the secure APIs enabled, then this is unlikely to work.
+- The following features are not supported
+  - Volume Lock/Unlock
+  - EQ
+  - Balance
+  - Follow
--- custom_components/acmax24/media_player.py
+++ custom_components/acmax24/media_player.py
@@ -1,6 +1,7 @@
 """Home Assistant Media Player for AVPro Edge AC-MAX-24 Audio Matrix"""
 
 import logging
+import asyncio
 
 import voluptuous as vol
 from homeassistant.components.media_player import PLATFORM_SCHEMA, MediaPlayerEntity
@@ -10,8 +11,7 @@ from homeassistant.const import (
     ATTR_ENTITY_ID,
     CONF_ENTITY_NAMESPACE,
     CONF_NAME,
-    CONF_HOSTNAME,
-    STATE_OFF,
+    CONF_HOST,
     STATE_ON,
     STATE_UNKNOWN,
 )
@@ -30,52 +30,22 @@ from .const import (
     # SERVICE_UNJOIN,
 )
 
-LOG = logging.getLogger(__name__)
+LOG: logging.Logger = logging.getLogger(__package__)
 
-# TODO: Review this; is there any value in this entity at all?
-SUPPORTED_AMP_FEATURES = MediaPlayerEntityFeature.TURN_ON | MediaPlayerEntityFeature.TURN_OFF | MediaPlayerEntityFeature.SELECT_SOURCE
+SUPPORTED_AMP_FEATURES = MediaPlayerEntityFeature.SELECT_SOURCE
 
 SUPPORTED_ZONE_FEATURES = (
     MediaPlayerEntityFeature.SELECT_SOURCE
-    # MediaPlayerEntityFeature.VOLUME_MUTE
-    # | SUPPORT_VOLUME_SET
-    # | SUPPORT_VOLUME_STEP
-    # | SUPPORT_TURN_ON
-    # | SUPPORT_TURN_OFF
-)
-
-CONF_SERIAL_NUMBER = "serial_number"  # allow for true unique id
-CONF_SOURCES = "sources"
-CONF_ZONES = "zones"
-CONF_DEFAULT_SOURCE = "default_source"
-
-SOURCE_IDS = vol.All(vol.Coerce(int), vol.Range(min=1, max=24))
-SOURCE_SCHEMA = vol.Schema(
-    {vol.Required(CONF_NAME, default="Unknown Source"): cv.string}
-)
-
-ZONE_IDS = vol.All(
-    vol.Coerce(int),
-    vol.Any(
-        vol.Range(min=1, max=24),
-    ),
-)
-
-# TODO: Read this from the acmax24 api
-ZONE_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_NAME, default="Audio Zone"): cv.string,
-        vol.Optional(CONF_DEFAULT_SOURCE): cv.positive_int,
-    }
+    | MediaPlayerEntityFeature.VOLUME_MUTE
+    | MediaPlayerEntityFeature.VOLUME_SET
+    | MediaPlayerEntityFeature.VOLUME_STEP
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_NAME, default="AVPro Edge AC-MAX-24"): cv.string,
-        vol.Required(CONF_HOSTNAME): cv.string,
+        vol.Required(CONF_HOST): cv.string,
         vol.Optional(CONF_ENTITY_NAMESPACE, default="acmax24"): cv.string,
-        vol.Required(CONF_ZONES): vol.Schema({ZONE_IDS: ZONE_SCHEMA}),
-        vol.Required(CONF_SOURCES): vol.Schema({SOURCE_IDS: SOURCE_SCHEMA}),
     }
 )
 
@@ -88,22 +58,37 @@ async def async_setup_platform(
     hass: HomeAssistantType, config, async_add_entities, discovery_info=None
 ):
     """Set up the AVPro Edge Audio Matrix platform."""
-    hostname = config.get(CONF_HOSTNAME)
+    hostname = config.get(CONF_HOST)
     namespace = config.get(CONF_ENTITY_NAMESPACE)
     entities = []
     matrix = None
+    matrix_entity = None
+
+    async def notify_callback():
+        if matrix_entity:
+            await matrix_entity.schedule_ha_update()
 
     try:
-        hostname = config.get(CONF_HOSTNAME)
-        matrix = ACMax24(hostname)
+        LOG.info("Setting up %s platform", namespace)
+        matrix = ACMax24(hostname, notify_callback)
+        LOG.debug("Matrix created, about to start")
+        
         await matrix.start()
-    except Exception:
-        LOG.error(f"Error initializing ACMax24 matrix at {hostname}")
+        LOG.debug("Started")
+
+        await hass.async_add_executor_job(matrix.update)
+        LOG.info("Initial update complete")
+    except Exception as e:
+        LOG.error(f"Error initializing ACMax24 matrix at {hostname} {e}")
+        raise PlatformNotReady
+
+    ready = await matrix.wait_for_initial_state(5)
+    if not ready:
+        LOG.warn("Initial state not available within timeout, not ready to start platform")
         raise PlatformNotReady
 
     sources = {
-        # JM: sources come from here; move to pulling them from the library
-        source_id: extra[CONF_NAME] for source_id, extra in config[CONF_SOURCES].items()
+        source.index: source.label for source in  matrix.get_enabled_inputs()
     }
 
     matrix_name = config.get(CONF_NAME)
@@ -111,23 +96,19 @@ async def async_setup_platform(
     LOG.info(
         f"Creating zone media players for {namespace} '{matrix_name}'; sources={sources}"
     )
-
-    # TODO: Switch to this!
-    # for output in matrix.get_enabled_outputs():
-    #     entities.append(
-    #         ZoneMediaPlayer(
-    #             namespace, matrix_name, matrix, sources, output
-    #         )
-    #     )
-    # for zone_id, extra in config[CONF_ZONES].items():
-    #     entities.append(
-    #         ZoneMediaPlayer(
-    #             namespace, matrix_name, matrix, sources, zone_id, extra[CONF_NAME]
-    #         )
-    #     )
+ 
+    for output in matrix.get_enabled_outputs():
+        LOG.debug("Adding ZoneMediaPlayer for %s", output)
+        entities.append(
+            # TODO: ZMP should set its own name
+            ZoneMediaPlayer(
+                namespace, matrix_name, matrix, sources, output
+            )
+        )
 
     # Add the master Media Player for the main control unit, with references to all the zones
-    entities.append(ACMax24Entity(namespace, matrix_name, matrix, sources, entities))
+    matrix_entity = ACMax24Entity(hass, namespace, matrix_name, matrix, sources, entities)
+    entities.append(matrix_entity)
 
     async_add_entities(entities, True)
 
@@ -158,11 +139,13 @@ async def async_setup_platform(
 class ACMax24Entity(MediaPlayerEntity):
     """Representation of the entire ACMax24 matrix."""
 
-    def __init__(self, namespace, name, matrix, sources, zone_players):
+    def __init__(self, hass, namespace, name, matrix, sources, zone_players):
+        self._hass = hass
         self._name = name
         self._matrix = matrix
         self._zone_players = zone_players
 
+        # TODO: Refactor the code that depends on these mappings; the acmax24 library can handle all this directly.
         self._source_id_to_name = sources  # [source_id]   -> source name
         self._source_name_to_id = {
             v: k for k, v in sources.items()
@@ -180,8 +163,15 @@ class ACMax24Entity(MediaPlayerEntity):
 
     async def async_update(self):
         """Retrieve the latest state from the amp."""
-        LOG.debug("async_update() is empty")
-        return
+        LOG.debug("async_update scheduling executor job to refresh labels")
+        try:
+            await self.hass.async_add_executor_job(self._matrix.update)
+        except Exception as e:
+            LOG.error("async_update encountered exception %s", e)
+        LOG.debug("async_update completed label refresh")
+
+    async def schedule_ha_update(self):
+        self.async_schedule_update_ha_state(force_refresh=True)
 
     @property
     def unique_id(self):
@@ -220,16 +210,6 @@ class ACMax24Entity(MediaPlayerEntity):
         for zone in self._zone_players:
             await zone.async_select_source(source)
 
-    # TODO: Why declare this feature, if there's nothing that happens?
-    async def async_turn_on(self):
-        """Turn the media player on."""
-        LOG.debug(f"Turning ON amp: {self._name}")
-
-    # TODO: Why declare this feature, if there's nothing that happens?
-    async def async_turn_off(self):
-        """Turn the media player off."""
-        LOG.debug(f"Turning OFF amp: {self._name}")
-
     @property
     def icon(self):
         return "mdi:speaker"
@@ -238,17 +218,17 @@ class ACMax24Entity(MediaPlayerEntity):
 class ZoneMediaPlayer(MediaPlayerEntity):
     """Representation of a matrix amplifier zone."""
 
-    def __init__(self, namespace, matrix_name, matrix, sources, zone_id, zone_name):
+    def __init__(self, namespace, matrix_name, matrix, sources, output):
         """Initialize new zone."""
         self._matrix = matrix
         self._matrix_name = matrix_name
-        self._name = zone_name
-        self._zone_id = zone_id
-        self._matrix_output = matrix.get_output(zone_id)
+        self._name = output.label
+        self._output = output
+        # TODO: Rename to output_id
+        self._zone_id = output.index
+        self._matrix_output = matrix.get_output(output.index)
 
-        # FIXME: since this should be a logical media player...why is it not good enough for the user
-        # specified name to represent this?  Other than it could be changed...
-        self._unique_id = f"{DOMAIN}_{matrix_name}_zone_{zone_id}".lower().replace(
+        self._unique_id = f"{DOMAIN}_{matrix_name}_zone_{output.index}".lower().replace(
             " ", "_"
         )
 
@@ -257,7 +237,7 @@ class ZoneMediaPlayer(MediaPlayerEntity):
         self._status = {}
         self._status_snapshot = None
 
-        self._source = None
+        self._source = None # xxx UPDATE THIS # TODO:
         self._source_id_to_name = sources  # [source_id]   -> source name
         self._source_name_to_id = {
             v: k for k, v in sources.items()
@@ -267,51 +247,13 @@ class ZoneMediaPlayer(MediaPlayerEntity):
         self._source_names = sorted(
             self._source_name_to_id.keys(), key=lambda v: self._source_name_to_id[v]
         )
-        # TODO: Ideally the source order could be overridden in YAML config (e.g. TV should appear first on list).
-        #       Optionally, we could just sort based on the zone number, and let the user physically wire in the
-        #       order they want (doesn't work for pre-amp out channel 7/8 on some Xantech)
 
     @property
     def zone_info(self):
         return f"{self._matrix_name} zone {self._zone_id} ({self._name})"
 
-    # TODO: We don't really need this; unless we use this to refresh input/output names in the API?
     async def async_update(self):
-        """Retrieve the latest state."""
-        try:
-            LOG.debug(f"Updating {self.zone_info}")
-            # TODO: Update state
-            # status = await self._amp.zone_status(self._zone_id)
-            # if not status:
-            #     return
-        except Exception as e:
-            # log up to two times within a specific period to avoid saturating the logs
-            @limits(calls=2, period=10 * MINUTES)
-            def log_failed_zone_update(e):
-                LOG.warning(f"Failed updating {self.zone_info}: {e}")
-
-            log_failed_zone_update(e)
-            return
-
-        LOG.debug(f"{self.zone_info} status update: {status}")
-        # self._status = status
-
-        # source_id = status.get("source")
-        # if source_id:
-        #     source_name = self._source_id_to_name.get(source_id)
-        #     if source_name:
-        #         self._source = source_name
-        #     else:
-        #         # TODO: Remove this if we start getting IO config from the device itself.
-        #         # sometimes the client may have not configured a source, but if the amplifier is set
-        #         # to a source other than one defined, go ahead and dynamically create that source. This
-        #         # could happen if the user changes the source through a different app or command.
-        #         source_name = f"Source {source_id}"
-        #         LOG.warning(
-        #             f"Undefined source id {source_id} for {self.zone_info}, adding '{source_name}'!"
-        #         )
-        #         self._source_id_to_name[source_id] = source_name
-        #         self._source_name_to_id[source_name] = source_id
+        pass
 
     @property
     def unique_id(self):
@@ -321,17 +263,12 @@ class ZoneMediaPlayer(MediaPlayerEntity):
     @property
     def name(self):
         """Return the name of the zone."""
-        return self._name
+        return self._output.label
 
     @property
     def state(self):
         """Return the powered on state of the zone."""
         return STATE_ON
-        # power = self._status.get("power")
-        # if power is not None and power is True:
-        #     return STATE_ON
-        # else:
-        #     return STATE_OFF
 
     @property
     def volume_level(self):
@@ -345,7 +282,6 @@ class ZoneMediaPlayer(MediaPlayerEntity):
     @property
     def is_volume_muted(self):
         """Boolean if output is currently muted."""
-        # FIXME: what about when volume == 0?
         return self._matrix_output.muted
 
     @property
@@ -356,7 +292,16 @@ class ZoneMediaPlayer(MediaPlayerEntity):
     @property
     def source(self):
         """Return the current input source of the device."""
-        return self._source
+        input = None
+        try:
+            input = self._matrix.get_input(self._matrix_output.input_channel)
+        except IndexError:
+            pass
+
+        if input:
+            return input.label
+        else:
+            return None
 
     @property
     def source_list(self):
@@ -392,65 +337,34 @@ class ZoneMediaPlayer(MediaPlayerEntity):
         LOG.info(f"Switching {self.zone_info} to source {source_id} ({source})")
         await self._matrix.change_input_for_output(self._zone_id, source_id)
 
-    # TODO: Remove this?
-    async def async_turn_on(self):
-        """Turn the media player on."""
-        LOG.debug(f"FIXME: Turning ON {self.zone_info}")
-        # await self._amp.set_power(self._zone_id, True)
-
-        # schedule a poll of the status of the zone ASAP to pickup volume levels/etc
-        # self.async_schedule_update_ha_state(force_refresh=True)
-
-    # TODO: Remove this?
-    async def async_turn_off(self):
-        """Turn the media player off."""
-        LOG.debug(f"FIXME: Turning OFF {self.zone_info}")
-        # await self._amp.set_power(self._zone_id, False)
-
-    # TODO: Implement this in the acmax24 library
+    # Note: When HA mutates the attributes, it immediately re-reads them -- but typically we haven't seen
+    # the update before it does, so it then takes another refresh cycle before it is observed.  There may be
+    # an element of the HA API I'm misunderstanding, but if not, then one option to address this is to track
+    # when we've sent a command, and not seen an update yet, and then wait briefly in the accessor functions
+    # for an update to arrive, before we return the current state
     async def async_mute_volume(self, mute):
         """Mute (true) or unmute (false) media player."""
-        LOG.debug(f"FIXME: Setting mute={mute} for zone {self.zone_info}")
-        #await self._amp.set_mute(self._zone_id, mute)
+        LOG.debug(f"Setting mute={mute} for zone {self.zone_info}")
+        await self._matrix.mute_output(self._zone_id, mute)
 
-    # TODO: Implement this
     async def async_set_volume_level(self, volume):
         """Set volume level, range 0—1.0"""
-        amp_volume = int(volume * MAX_VOLUME)
-        LOG.debug(
-            f"FIXME: Setting zone {self.zone_info} volume to {amp_volume} (HA volume {volume}"
-        )
-        # await self._amp.set_volume(self._zone_id, amp_volume)
+        LOG.debug(f"Setting zone {self.zone_info} volume to {volume}")
+        await self._matrix.set_output_volume(self.zone_info, int(volume * 100))
 
-    # TODO: Implement this
     async def async_volume_up(self):
         """Volume up the media player."""
-        LOG.debug(f"FIXME: async_volume_up")
-    
-        # volume = self._status.get("volume")
-        # if volume is None:
-        #     return
+        LOG.debug(f"async_volume_up")
+        await self._matrix.step_output_volume(self._zone_id, 5)
 
-        # FIXME: call the volume up API on the amp object, instead of manually increasing volume
-        # reminder the volume is on the amplifier scale (0-38), not Home Assistants (1-100)
-        # await self._amp.set_volume(self._zone_id, min(volume + 1, MAX_VOLUME))
-
-    # TODO: Implement this
     async def async_volume_down(self):
         """Volume down media player."""
         LOG.debug(f"FIXME: async_volume_down")
-        # volume = self._status.get("volume")
-        # LOG.debug(f"FIXME: async_volume_down")
-        # if volume is None:
-        #     return
-
-        # FIXME: call the volume down API on the amp object, instead of manually increasing volume
-        # reminder the volume is on the amplifier scale (0-38), not Home Assistants (1-100)
-        # await self._amp.set_volume(self._zone_id, max(volume - 1, 0))
+        await self._matrix.step_output_volume(self._zone_id, -5)
 
     @property
     def icon(self):
-        if self.is_volume_muted:
+        if self._output.muted:
             return "mdi:speaker-off"
         return "mdi:speaker"
 
